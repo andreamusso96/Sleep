ba19a0780858ccc863399d4272ea450386305281
andreamusso96 <andreamusso96@gmail.com>
2023-11-23 20:28:30 +0100

    Other issues with cluster_job_submitter.py

--- Sleep/cluster_job_submitter.py
+++ Sleep/cluster_job_submitter.py
@@ -8,7 +8,7 @@ import mobile_traffic as mt
 def submit_jobs():
     cities = [mt.City.BORDEAUX]
     for city in cities:
-        submit_command = f'sbatch --mem=65G --cpus-per-task=8 --time=08:00:00 --wrap="python -m cluster_run {city.value}"'
+        submit_command = f'sbatch --mem-per-cpu=8G --ntasks=1 --cpus-per-task=8 --time=08:00:00 --wrap="python -m cluster_run {city.value}"'
         print('SUBMITTING JOB WITH COMMAND: ', submit_command)
         process = subprocess.Popen(submit_command, shell=True)
         process.communicate()
