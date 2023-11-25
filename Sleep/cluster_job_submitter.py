0bb37b8ae2291461c134dec34d9530e2ed572c68
andreamusso96 <andreamusso96@gmail.com>
2023-11-25 10:22:20 +0100

    Memory issues with job

--- Sleep/cluster_job_submitter.py
+++ Sleep/cluster_job_submitter.py
@@ -8,7 +8,7 @@ import mobile_traffic as mt
 def submit_jobs():
     args = ['rca', 'nsi', 'other']
     for arg in args:
-        submit_command = f'sbatch --mem-per-cpu=2G --ntasks=1 --cpus-per-task=8 --time=14:00:00 --wrap="python -m cluster_run {arg}"'
+        submit_command = f'sbatch --mem-per-cpu=12G --ntasks=1 --cpus-per-task=8 --time=14:00:00 --wrap="python -m cluster_run {arg}"'
         print('SUBMITTING JOB WITH COMMAND: ', submit_command)
         os.system(submit_command)
         time.sleep(2)
